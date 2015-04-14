bfeab2d07f3206f403aeeb24d4bd828f5bf9cec8
Soul Raj Joshi <srj2120@columbia.edu>
2015-04-13 20:16:01 -0400

    answers shown

--- public/app.js
+++ public/app.js
@@ -1,2 +1,12 @@
 var app = angular.module("FlashCards", []);
+app.controller("FlashCardController", function($scope){
+	// we now have access to our scope using $scope.
+	$scope.flashCard = {
+		question: "What is Angular",
+		answers: [{text: "A front-end framework for great power!", correct: true},
+			{text: "Something lame, who cares, whatever.", correct: false},
+			{text: "Some kind of fish, right?", correct: false}]
+	};
+});
+
 
